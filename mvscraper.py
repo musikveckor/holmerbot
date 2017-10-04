0c720b0cc4f8ef8aaaa89211190330cd8d3c8c9e
Robert Alm <robertalm3@gmail.com>
2017-10-04 22:45:28 +0200

    More work on scraper

--- mvscraper.py
+++ mvscraper.py
@@ -1,6 +1,7 @@
 from HTMLParser import HTMLParser
 import urllib
 import re
+from datetime import datetime
 
 #opener =  urllib.FancyURLopener({})
 #url = "http://musikveckor.blogspot.se/"
@@ -8,20 +9,64 @@ import re
 #content = f.read()
 
 
+# Enum for playlist link types
+class PlaylistLinkType:
+    UNKNOWN     = -1
+    SPOTIFY_URI = 0
+    SPOTIFY_WEB = 1
+    YOUTUBE     = 2
+    SOUNDCLOUD  = 3
+
+
+class PlayListLink:
+    link = ""
+    linkType = None
+
+    def __init__(self, link, type):
+        self.link = link
+        self.linkType = type
+
+
+class Tag:
+    name = ""
+    attrs = []
+
+    def __init__(self, name, attrs):
+        self.name = name
+        self.attrs = attrs
+
+    def isLink(self):
+        return self.name == 'a' and self.getAttr('href') is not None
+
+    def hasClass(self, classSpec):
+        for a in self.attrs:
+            if a[0] == 'class':
+                if a[1].strip() == classSpec:
+                    return True
+        return False
+
+    def getAttr(self, attrName):
+        for a in self.attrs:
+            if a[0] == attrName:
+                return a[1]
+        return None
+
+
+class HTMLParserBase(HTMLParser):
+    def findAttrFromTag(self, detectTag, tag, attr, attrs):
+        return
+
+
 #
-class PageArchiveParser(HTMLParser):
-    postPageList = list()
+class BlogPostParser(HTMLParserBase):
+    title = ""
+    dateTime = None
+    post_html_content = ""
+    poster = ""
+    playlistLinks = list()
 
     def handle_starttag(self, tag, attrs):
-        if tag == 'a' and  attrs[0][0] == 'class' and attrs[0][1] == 'post-count-link':
-            m = re.match('http://musikveckor.blogspot.se/[0-9]*/[0-9]*/', attrs[1][1])
-            if m != None:
-                self.postPageList.append(m.group(0))
-                print m.group(0)
-
-#
-class MonthPageParser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
+        return
 
     def handle_endtag(self, tag):
         return
@@ -29,20 +74,55 @@ class MonthPageParser(HTMLParser):
     def handle_data(self, data):
         return
 
+
+#
+class PageArchiveParser(HTMLParserBase):
+    monthPageList = list()
+
+    def handle_starttag(self, tag, attrs):
+        tag = Tag(tag, attrs)
+        if tag.isLink() and tag.hasClass('post-count-link'):
+            m = re.match('http://musikveckor.blogspot.se/[0-9]*/[0-9]*/', tag.getAttr('href'))
+            if m is not None:
+                self.monthPageList.append(m.group(0))
+                print m.group(0)
+            else:
+                print "Skipping " + attrs[1][1]
+
+    def feed(self, data):
+        print "Parsing blog post archive..."
+        HTMLParser.feed(self, data)
+
+
+#
+class MonthPageParser(HTMLParserBase):
+    def handle_starttag(self, tag, attrs):
+        return
+
+    def handle_endtag(self, tag):
+        return
+
+    def handle_data(self, data):
+        return
+
+    def process(self, pageLink):
+        print "Parsing month page " + pageLink + "..."
+        #fancy = urllib.FancyURLopener({})
+        #pageFile = fancy.open(pageLink)
+        #monthPageContent = pageFile.read()
+        #HTMLParserBase.feed(self, monthPageContent)
+
+
 with open('mvroot.html') as f:
     content = f.read()
 
 # instantiate the parser and fed it some HTML
 pageArchiveParser = PageArchiveParser()
 pageArchiveParser.feed(content)
-print str(len(pageArchiveParser.postPageList)) + " pages to process"
+print str(len(pageArchiveParser.monthPageList)) + " pages to process"
 
 
-def processMonthPage(page):
-    fancy = urllib.FancyURLopener({})
-    pageFile = fancy.open(page)
-    pageContent = pageFile.read()
-
-for monthPage in pageArchiveParser.postPageList:
-    processMonthPage(monthPage)
+for monthPage in pageArchiveParser.monthPageList:
+    monthPageParser = MonthPageParser()
+    monthPageParser.process(monthPage)
 
